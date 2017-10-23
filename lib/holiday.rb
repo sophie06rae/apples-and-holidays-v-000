5ed7f04d2f117fde06382e032cadda77d9e9b4a6
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-23 08:54:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,7 +96,7 @@ def all_holidays_with_bbq(holiday_hash)
 bbq_array = []
 holiday_hash.each do |season, holiday|
 holiday.map do |holiday, items|
-  items.find "BBQ"
+  items.find{ |word| word == "BBQ"}
   return holiday
 end
 end
