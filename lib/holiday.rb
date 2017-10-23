7d6d353b2f04d9799235a459f9d2307fcab3d9ca
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-23 08:50:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 bbq_array = []
 holiday_hash.each do |season, holiday|
-if holiday.include? "BBQ"
+holiday.each do |holiday, items|
+if items.include? "BBQ"
   return holiday 
 end
 end
