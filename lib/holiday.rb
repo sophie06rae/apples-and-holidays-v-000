632343666bce9c10774826885fdaecbd462924e1
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-23 08:25:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,5 +94,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |key, value|
+end 
   binding.pry
 end
